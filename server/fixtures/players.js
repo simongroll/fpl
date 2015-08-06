b4ced41f55feaabc0135c27332af99d43ead21d4
Simon Groll <simongroll@gmail.com>
2015-08-06 09:25:30 +0200

    Complete team.json. Deny updates/insertions/removals from client.

--- server/fixtures/players.js
+++ server/fixtures/players.js
@@ -1,14 +1,14 @@
-Meteor.startup(function() {
+// Meteor.startup(function() {
 
-    var max_id = 533;
+//     var max_id = 533;
 
-    if (Players.find().count() === 0) {
-        for (var i = 1; i < max_id + 1; i++) {
-            Meteor.call("getPlayerData", i, function(err) {
-                if (err) {
-                    console.log("getPlayerData failed with index = " + i);
-                }
-            });
-        }
-    }
-});
+//     if (Players.find().count() === 0) {
+//         for (var i = 1; i < max_id + 1; i++) {
+//             Meteor.call("getPlayerData", i, function(err) {
+//                 if (err) {
+//                     console.log("getPlayerData failed with index = " + i);
+//                 }
+//             });
+//         }
+//     }
+// });
