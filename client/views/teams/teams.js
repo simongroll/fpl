363c0fb172835bc0c3989efeac2f3d593ba37501
Simon Groll <simongroll@gmail.com>
2015-08-28 15:29:11 +0200

    Gameweek view.

--- client/views/teams/teams.js
+++ client/views/teams/teams.js
@@ -22,24 +22,24 @@ Template.teams.rendered = function() {
 };
 
 Template.teams.events({
-    'click button': function() {
+    'click button#update': function() {
         Session.set("isUpdating", true);
         var teams = Teams.find().fetch();
         var teamCount = teams.length;
         var counter = 0;
 
-        var teams = _.each(Teams.find().fetch(), function(team){
+        var teams = _.each(Teams.find().fetch(), function(team) {
             var oldScore = team.score;
-            console.log("Calling 'update' for "+team.name);
-            Meteor.call("loadOneTeamData", team._id, function(err, res){
-                console.log("Team: "+team.name);
-                console.log("Old score: "+oldScore);
-                console.log("New score: "+res);
+            console.log("Calling 'update' for " + team.name);
+            Meteor.call("loadOneTeamData", team._id, function(err, res) {
+                console.log("Team: " + team.name);
+                console.log("Old score: " + oldScore);
+                console.log("New score: " + res);
                 console.log();
                 counter++;
 
-                console.log("counter ="+counter);
-                console.log("teamCount ="+teamCount);
+                console.log("counter =" + counter);
+                console.log("teamCount =" + teamCount);
 
                 if (counter == teamCount) {
                     Session.set("isUpdating", false);
@@ -145,3 +145,189 @@ Template.detailed.helpers({
         return _.first(sortedForwards, 2);
     }
 });
+
+Template.gameweeks.created = function() {
+    var maxGameweek = this.data.teams().fetch()[0].players[0].gameweeks.length;
+    Session.set("gameweek", maxGameweek);
+    Session.set("maxGameweek", maxGameweek);
+};
+
+Template.gameweeks.events({
+    'click button.change-gameweek': function(evt) {
+        Session.set("gameweek", evt.target.innerText);
+    }
+});
+
+Template.gameweeks.helpers({
+    currentGameweek: function() {
+        return Session.get("gameweek");
+    },
+    changeGameweekButtons: function() {
+        var maxGameweek = Session.get("maxGameweek");
+        var currentGameweek = Session.get("gameweek");
+        var buttonHTML = "";
+        for (var i = 1; i < maxGameweek + 1; i++) {
+            if (i === currentGameweek) {
+            buttonHTML += '<button type="button" class="btn btn-info btn-flat change-gameweek">' + i + '</button>';
+            }
+            else {
+            buttonHTML += '<button type="button" class="btn btn-default btn-flat change-gameweek">' + i + '</button>';
+            }
+        };
+        return buttonHTML;
+    },
+    gk: function() {
+        var goalies = _.where(this.players, {
+            position: "Goalkeeper"
+        });
+        var sortedGoalies = _.sortBy(goalies, 'points').reverse();
+
+        var goalie = _.first(sortedGoalies);
+        var gw = Session.get("gameweek");
+        return {
+            name: goalie.name,
+            points: goalie.gameweeks[gw - 1].score
+        };
+    },
+
+    defenders: function() {
+        var defenders = _.where(this.players, {
+            position: "Defender"
+        });
+        var sortedDefenders = _.sortBy(defenders, 'points').reverse();
+
+        var df = [];
+        var gw = Session.get("gameweek");
+
+        _.each(_.first(sortedDefenders, 4), function(defender) {
+            df.push({
+                name: defender.name,
+                points: defender.gameweeks[gw - 1].score
+            });
+        })
+        return df;
+    },
+
+    midfielders: function() {
+        var midfielders = _.where(this.players, {
+            position: "Midfielder"
+        });
+        var sortedMidfielders = _.sortBy(midfielders, 'points').reverse();
+
+        var mf = [];
+        var gw = Session.get("gameweek");
+
+        _.each(_.first(sortedMidfielders, 4), function(midfielder) {
+            if (midfielder.gameweeks[gw - 1]) {
+
+                mf.push({
+                    name: midfielder.name,
+                    points: midfielder.gameweeks[gw - 1].score
+                });
+            } else {
+                mf.push({
+                    name: midfielder.name,
+                    points: 0
+                });
+            }
+        })
+        return mf;
+    },
+
+    forwards: function() {
+        var forwards = _.where(this.players, {
+            position: "Forward"
+        });
+
+        var cf = [];
+        var gw = Session.get("gameweek");
+
+        _.each(_.first(_.sortBy(forwards, 'points').reverse(), 2), function(forward) {
+            cf.push({
+                name: forward.name,
+                points: forward.gameweeks[gw - 1].score
+            });
+        });
+        return cf;
+    },
+
+    gameweekTotal: function() {
+        var gw = Session.get("gameweek");
+
+        var goalies = _.where(this.players, {
+            position: "Goalkeeper"
+        });
+        var sortedGoalies = _.sortBy(goalies, 'points').reverse();
+        var goalie = _.first(sortedGoalies);
+        var gkScore = goalie.gameweeks[gw - 1].score;
+
+        var defenders = _.where(this.players, {
+            position: "Defender"
+        });
+        var sortedDefenders = _.sortBy(defenders, 'points').reverse();
+        var dfScore = 0;
+        _.each(_.first(sortedDefenders, 4), function(defender) {
+            if (defender.gameweeks[gw - 1]) {
+                dfScore += defender.gameweeks[gw - 1].score;
+            }
+        });
+
+        var midfielders = _.where(this.players, {
+            position: "Midfielder"
+        });
+        var sortedMidfielders = _.sortBy(midfielders, 'points').reverse();
+        var mfScore = 0;
+        _.each(_.first(sortedMidfielders, 4), function(midfielder) {
+            if (midfielder.gameweeks[gw - 1]) {
+                mfScore += midfielder.gameweeks[gw - 1].score
+            }
+        });
+
+        var forwards = _.where(this.players, {
+            position: "Forward"
+        });
+        var cfScore = 0;
+        _.each(_.first(_.sortBy(forwards, 'points').reverse(), 2), function(forward) {
+            if (forward.gameweeks[gw - 1]) {
+                cfScore += forward.gameweeks[gw - 1].score;
+            }
+        });
+
+
+        return gkScore + dfScore + mfScore + cfScore;
+    }
+});
+/*
+Template.gameweeks2.helpers({
+    gk: function() {
+        var goalies = _.where(this.players, {
+            position: "Goalkeeper"
+        });
+        var sortedGoalies = _.sortBy(goalies, 'points').reverse();
+        return _.first(sortedGoalies);
+    },
+
+    defenders: function() {
+        var defenders = _.where(this.players, {
+            position: "Defender"
+        });
+        var sortedDefenders = _.sortBy(defenders, 'points').reverse();
+        return _.first(sortedDefenders, 4);
+    },
+
+    midfielders: function() {
+        var midfielders = _.where(this.players, {
+            position: "Midfielder"
+        });
+        var sortedMidfielders = _.sortBy(midfielders, 'points').reverse();
+        return _.first(sortedMidfielders, 4);
+    },
+
+    forwards: function() {
+        var forwards = _.where(this.players, {
+            position: "Forward"
+        });
+        var sortedForwards = _.sortBy(forwards, 'points').reverse();
+        return _.first(sortedForwards, 2);
+    }
+});*/
--- server/api.js
+++ server/api.js
@@ -69,6 +69,14 @@ Meteor.methods({
 
                 var response = HTTP.get('http://fantasy.premierleague.com/web/api/elements/' + player.id);
 
+                var GW = [];
+                _.each(response.data.fixture_history.all, function(gw, index) {
+                    GW.push({
+                        i: index,
+                        score: gw[gw.length - 1]
+                    });
+                });
+
                 Teams.update({
                     "_id": team._id,
                     'players.id': player.id
@@ -88,7 +96,8 @@ Meteor.methods({
                         'players.$.in_dream_team': response.data.in_dreamteam,
                         'players.$.yellows': response.data.yellow_cards,
                         'players.$.reds': response.data.red_cards,
-                        'players.$.saves': response.data.saves
+                        'players.$.saves': response.data.saves,
+                        'players.$.gameweeks': GW
                     }
                 });
 
@@ -115,20 +124,19 @@ Meteor.methods({
 
             var newScore = gk + df + mf + cf;
             console.log(team.name);
-            console.log("gk = "+gk);
-            console.log("df = "+df);
-            console.log("mf = "+mf);
-            console.log("cf = "+cf);
-            console.log("new score = "+newScore);
+            console.log("gk = " + gk);
+            console.log("df = " + df);
+            console.log("mf = " + mf);
+            console.log("cf = " + cf);
+            console.log("new score = " + newScore);
 
             Teams.update({
-                _id: team._id
+                "_id": team._id
             }, {
                 $set: {
                     score: newScore
                 }
             });
-
 
             return newScore;
 
